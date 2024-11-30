800ad6a9b2cd223e5570bbe4260ca5a6c1123c69
ceovejero <cesarovejero@gmail.com>
2024-11-29 21:39:10 -0300

    Resuelto error de medicion humedad

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sensor_dht22 import medir_temperatura, medir_humedad
 
 while True:
     temp = medir_temperatura()
+    time.sleep(0.5) # Evita errores en la toma de mediciones
     hum = medir_humedad()
 
     if temp is not None:
